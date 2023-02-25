76d6ee9b4f35335e336dbdffd2a0b000c120258b
PyoChangYeong <game1425@naver.com>
2023-02-26 01:34:07 +0900

    Feat : 02.26 TestApplication 오류 수정

--- src/test/java/com/sparta/schedule/ScheduleApplicationTests.java
+++ src/test/java/com/sparta/schedule/ScheduleApplicationTests.java
@@ -1,11 +1,9 @@
 package com.sparta.schedule;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@SpringBootApplication
 class ScheduleApplicationTests {
 
     @Test
